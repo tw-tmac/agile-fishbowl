f25a84b5663e4ca66b2a09a598effca4682f926a
th0f7h5 <tmac@thoughtworks.com>
2014-12-25 19:24:21 -0500

    Thien: Upgrade to bootstrap 3.3.1, using erb instead of haml

--- agile_fishbowl.rb
+++ agile_fishbowl.rb
@@ -1,14 +1,11 @@
 require 'rubygems'
 require 'sinatra'
-#require 'sinatra_more' # glob all sinatra_more 
-#require 'sinatra_more/markup_plugin' # or require 'sinatra_more/markup_plugin' for precise inclusion
-require 'haml'
-#require 'datamapper'
 require 'dm-core'
 require 'dm-timestamps'
 require 'dm-postgres-adapter'
 require 'dm-migrations'
-require 'pony'
+#we'll need it later on for email
+#require 'pony'
 require 'dm-types'
 
 $DB_USERNAME = "twer"
@@ -23,12 +20,7 @@ DataMapper::setup(:default, $DATABASE_URL)
 require './user'
 
 get '/' do
-	@var = "This is a variable from somewhere else"
-  haml :index
-end
-
-get '/about' do
-  haml :about
+  erb :index
 end
 
 set :bind, '0.0.0.0'
