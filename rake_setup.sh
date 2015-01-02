bcab7295f1a5d8df41aacd9bb239ce3fd2a8c9ae
th0f7h5 <tmac@thoughtworks.com>
2015-01-02 00:55:58 -0500

    Thien: add script for ci server setup

---
+++ rake_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ 
+RAILS_ROOT=$(cd `dirname $0`/.. && pwd)
+ 
+unset GEM_HOME
+unset GEM_PATH
+ 
+export GEM_HOME=$RAILS_ROOT/vendor
+export GEM_PATH=$GEM_HOME
+export PATH=$GEM_HOME/bin:$PATH
+ 
+mkdir -p $GEM_HOME
+ 
+cd $RAILS_ROOT
+ 
+ruby install sinatra dm-core dm-migrations dm-types dm-aggregates
